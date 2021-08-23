93e16f84a56e9d606d3c1c68b986f09932aa43b9
Aditya Jamuar <adityaj@geekyants.com>
2021-08-23 18:15:52 +0530

    Starter kit for CRA done

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import StarterIntro from "./screens/StarterIntro";
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/"></Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/">
+          <StarterIntro />
+        </Route>
+      </Switch>
     </Router>
   );
 }
